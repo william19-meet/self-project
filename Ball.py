f8fbaab44aa2e9826edf983d12f47380df6a30d4
Student <william19-meet>
2018-01-15 19:19:14 +0200

    Balls

--- Ball.py
+++ Ball.py
@@ -1,5 +1,6 @@
-from turtle import Turtle
-turtle.penup()
+import turtle
+from turtle import *
+
 class Ball(Turtle):
     def __init__(self,x,y,dx,dy,r,color):
         Turtle.__init__(self, shape = 'circle')
@@ -11,25 +12,42 @@ class Ball(Turtle):
         self.shapesize = self.r/10
         self.color(color)
 
+        turtle.penup()
+        turtle.goto(x,y)
+
         
     def move(self,screen_width,screen_height):
         current_x = self.x
         newx = current_x - self.dx
+        
         current_y = self.x
         newy = current_y - self.dy
+        
         top = height/2
         bottom = -(height/2)
         right = width/2
         left = -(width/2)
+        
         rightside = newx + self.r
         leftside = newx - self.r
         upside = newy + self.r
         bottomside = newy - self.r
-        
+
+
+        if right_side_ball >= screen_width/2:
+            newx = current_x
+
+        if left_side_ball <= screen_width/-2:
+            newx = current_x
+
+        if top_side_ball >= screen_height/2:
+            newy = current_y
+
+        if bottom_side_ball <= screen_height/-2:   
+            newy = current_y
+
         
     
-        
-    
-turtle.goto(x,y)
+        turtle.goto(x,y)
 
 turtle.mainloop()
---
+++ agario.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+import random
+from Ball import Ball
+
+
+RUNNING = True
+SLEEP = 0.0077
+SCREEN_WIDTH = turtle.getcanvas().winfo_width()/2
+SCREEN_HEIGHT = turtle.getcanvas().winfo_height()/2
+
+
+turtle.tracer(1,0)
+turtle.hideturtle()
